de561d5435c3ab3501eddd4a68c52f263dccb024
urieluard <uriel.uard@gmail.com>
2021-02-21 11:11:44 -0600

    Se comentan las funciones y se modifican algunas

--- src/pipeline/ingesta_almacenamiento.py
+++ src/pipeline/ingesta_almacenamiento.py
@@ -8,29 +8,40 @@ from datetime import datetime
 
 from src.utils.general import get_api_token, get_s3_credentials
 
+''' Variables de entorno'''
 socrata_domain = "data.cityofchicago.org"
 socrata_ds_id = "4ijn-s7e5"
 path = os.path.realpath('conf/local/credentials.yaml')
+delta_date = '2021-02-15T00:00:00.000'
 
 # ================================= FUNCTION  ================================= #
 
 def get_client():
+    '''Función que devuelve el cliente contenido en el archivo credentials.yaml
+       (este cliente será utilizado para conectarse a la App de Chicago Food Inspections)
+    '''
     token = get_api_token(path)
     #token = get_api_token('/mnt/c/Users/jlrza/jlrzcdir/2-data-product-architecture/rep-dpa-proy/conf/local/credentials.yaml')
     client = Socrata(socrata_domain, token['api_token'])  #timeout=10)
     
     return client
 
-
 # ================================= FUNCTION 2 ================================= #
 
-def ingesta_inicial(client, limit = 300000):
+def ingesta_inicial(socrata_ds_id, limit = 300000):
+    '''Función que se conecta a la App de Chicago Food Inspections y devuelve
+       una consulta histórica de tamaño "limit". Si no hay ese número de registros
+       devolverá el máximo que haya en la Base de datos de la API.
+    '''
     return client.get(socrata_ds_id, limit = limit)
 
-
 # ================================= FUNCTION 3 ================================= #
+
 def get_s3_resource():
     s3_credentials = get_s3_credentials(path)
+    '''Función que se crea un objeto "s3" para conectarse al servicio s3 de AWS
+       a partir de las crdenciales que se encuentran en credentials.yaml
+    '''
     #s3_credentials = get_s3_credentials('/mnt/c/Users/jlrza/jlrzcdir/2-data-product-architecture/rep-dpa-proy/conf/local/credentials.yaml')
     
     session = boto3.Session(
@@ -42,24 +53,39 @@ def get_s3_resource():
     
     return s3
 
-
 # ================================= FUNCTION 4 ================================= #
 
-def guardar_ingesta(my_bucket, bucket_path, data):
+def guardar_ingesta(my_bucket, bucket_path):
+    '''Función que recibe de argumentos el nombre del bucket s3 en el que se quiere 
+       almacenar y en la ruta en la que se guardará la información que se consulte 
+       de la API de Chicago food inspections.
+       Si 
+         -bucket_path- = ingestion/initial/ 
+         Se hará la consulta de todo lo que se encuentre en la BD, desde la fecha en
+         que se corre la función y hasta el valor de registros que se estableció 
+         en la variable -límite- de la función ingesta_inicial.
+       Si
+         -bucket_path- = ingestion/consecutive/ 
+         Se hará la consulta de todo lo que se encuentre en la BD, desde la fecha que se
+         establezca en la variable -delta_date- y hasta el valor de registros que se estableció 
+         en la variable -límite- de la función -ingesta_consecutiva-.
+    '''
     s3 = get_s3_resource()
-    pickle_buffer = pkl.dumps(data)
     fecha = str(datetime.date(datetime.now()))
     
     if bucket_path == "ingestion/initial":
         nom_file = "ingestion/initial/" + "historic-inspections-" + fecha + ".pkl"
+        data = ingesta_inicial(socrata_ds_id, limit = 300000) 
+        pickle_buffer = pkl.dumps(data)
     else:
         nom_file =  "ingestion/consecutive/" + "consecutive-inspections-" + fecha + ".pkl"
+        data = ingesta_consecutiva(socrata_ds_id, delta_date, limit = 1000) 
+        pickle_buffer = pkl.dumps(data)
     
     s3.Object(my_bucket, nom_file).put(Body = pickle_buffer) 
     return
 
-
 # ================================= FUNCTION 5 ================================= #
 
-def ingesta_consecutiva(client, delta_date = '2021-02-15T00:00:00.000', limit = 1000):
+def ingesta_consecutiva(socrata_ds_id, delta_date = '2021-02-15T00:00:00.000', limit = 1000):
     return client.get(socrata_ds_id, where = "inspection_date > " + "'" + delta_date + "'", limit = limit)
--- src/utils/general.py
+++ src/utils/general.py
@@ -5,7 +5,10 @@ import yaml
 # ================================= FUNCTION 1 ================================= #
 
 def read_yaml(credentials_file):
-    """ load yaml cofigurations """
+    """ Carga las credenciales que se tengan en el path especificado,
+        para esta  aplicación se debe colocar en la ruta:
+        /conf/local/credentials.yaml
+    """
     
     config = None
     try: 
@@ -20,6 +23,13 @@ def read_yaml(credentials_file):
 # ================================= FUNCTION 2 ================================= #
 
 def get_s3_credentials(credentials_file):
+	""" Lee el archivo credentials.yaml y devuelve las credenciales 
+	    del cliente designado para conectarse al servicio s3 de AWS.
+	    Se requiere que el archivo credentials.yaml contenga:
+	    s3:
+	    AWSAccessKeyId= -tu id-
+	    AWSSecretKey= -tu llave-
+	"""
     s3_credentials = read_yaml(credentials_file)['s3']
     
     return s3_credentials
@@ -28,7 +38,15 @@ def get_s3_credentials(credentials_file):
 # ================================= FUNCTION 3 ================================= #
 
 def get_api_token(credentials_file):
-    
+	""" Lee el archivo credentials.yaml y devuelve las credenciales 
+	    del cliente designado para conectarse al servicio de la API 
+	    de Chicago Food Inspections.
+	    Se requiere que el archivo credentials.yaml contenga la siguiente 
+	    información:
+	    food_inspections:
+	    AppToken= -tu Token público-
+	    SecretToken= -tu Token secreto-
+	 """
     token = read_yaml(credentials_file)['food_inspections']
     
     return token
