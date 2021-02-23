98f3f18803eea765f93084eee0fdd9fec199c6ae
urieluard <uriel.uard@gmail.com>
2021-02-22 22:03:44 -0600

    actualiza la función

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,6 +2,9 @@
 
 import os
 
+key1 = "ingestion/initial"
+key2 = "ingestion/consecutive"
+
 socrata_domain = "data.cityofchicago.org"
 socrata_ds_id = "4ijn-s7e5"
 path = os.path.realpath('conf/local/credentials.yaml')
