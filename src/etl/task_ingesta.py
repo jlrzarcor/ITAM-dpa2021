e7d96e32f365ed5b385076b2fc5c8a48ca71daae
jlrzarcor <jlrzarate@hotmail.com>
2021-03-14 22:03:36 -0600

    prefinal version fully functional, #7

--- src/etl/task_ingesta.py
+++ src/etl/task_ingesta.py
@@ -1,22 +1,11 @@
 # ================================= LIBRARIES  ================================= #
 
+# importing packages
 import luigi
-import luigi.contrib.s3
-import boto3
-import os
 import json
-import pickle as pkl
-
-from sodapy import Socrata
 from datetime import datetime
 
-
-# Variables de entorno que se cargan por default al cargar la librería
-# ingesta_almacenamiento.py
-
 # importing custom libraries
-import src.utils.general as gral
-import src.utils.constants as ks
 import src.pipeline.ingesta_almacenamiento as ial
 
 
@@ -29,28 +18,28 @@ class TaskIngest(luigi.Task):
     year = luigi.IntParameter(default = todate.year)
     month = luigi.IntParameter(default = todate.month)
     day = luigi.IntParameter(default = todate.day)
+    
     flg_i0_c1 = luigi.IntParameter(default = 1)
         
     def run(self):
     # Formatting date parameters into date-string
-        str_date = datetime.date(datetime(self.year,self.month,self.day))
+        str_date = str(datetime.date(datetime(self.year, self.month, self.day))) + 'T00:00:00.000'
         
     # SODAPY. Connecting client to Socrata and Ingestion -> json.dumps
         client = ial.get_client()
-            # Debug print: print("Raise Flag Success:", client)
 
         if self.flg_i0_c1 == 0:
-            data_i = ial.ingesta_inicial(client, str_date + 'T00:00:00.000', 300)
+            data_i = ial.ingesta_inicial(client, delta_date = str_date , limit = 300)
         else:
-            data_i = ial.ingesta_consecutiva(client, str_date + 'T00:00:00.000', 3)
+            data_i = ial.ingesta_consecutiva(client, delta_date = str_date, limit = 3)
         
         json_data = json.dumps(data_i, indent=4)
         
-            # Debug print: print("Raise Flag", data_i, "X-Man", ks.path, "json", json_data)
     # Call output
         with self.output().open('w') as output_file:
             output_file.write(json_data)
         
-    def output(self):    
-        output_path = "./src/temp/TYPINGST={}/ingesta.json".format(self.flg_i0_c1)
+    def output(self):
+        str_file = "ingesta" + str(datetime.date(datetime(self.year,self.month,self.day)))        
+        output_path = "./src/temp/TYPINGST={}/{}".format(self.flg_i0_c1, str_file)
         return luigi.local_target.LocalTarget(path=output_path)
