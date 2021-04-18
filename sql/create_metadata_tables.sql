386f522a65b8b5f95b21007369ad5d338cfe35c6
Jose Luis R. Zarate <jlrzarate@hotmail.com>
2021-04-18 18:30:10 -0500

    restore .sql file

---
+++ sql/create_metadata_tables.sql
@@ -0,0 +1,63 @@
+/*
+ * ITAM-Data - Product Architecture - Master Liliana Millán
+ * Project: Chicago Food Inspections - Team 05
+ * Script: Create tables for metadata...
+ */
+
+
+
+/* ================================= metadata task ingestion ================================= */
+
+DROP TABLE IF EXISTS metadata.ingestion;
+CREATE TABLE metadata.ingestion (
+  fecha date,
+  param_exec json,
+  usuario varchar(10),
+  num_regs_ing int
+);
+COMMENT ON TABLE metadata.ingestion IS 'Almacena metadata de los datos ingestados';
+
+<<<<<<< HEAD
+/* ================================ testing tables ================================= */
+
+DROP TABLE IF EXISTS metadata.test;
+CREATE TABLE metadata.test (
+  num_regs_ing int
+);
+COMMENT ON TABLE metadata.test IS 'Almacena metadata de los datos ingestados';
+=======
+
+/* ================================= metadata cleaning ================================= */
+
+DROP TABLE IF EXISTS metadata.cleaning;
+CREATE TABLE metadata.cleaning (
+  num_registros_antes_limpieza int
+  num_registros_despues_limpieza int
+  valores_nulos int
+  tamano_df_limpio int
+  fecha_ejecucion date
+  executer varchar(10)
+);
+COMMENT ON TABLE metadata.cleaning IS 'Almacena metadata de los datos ingestados';
+
+
+/* ================================= metadata feature engineering ================================= */
+
+DROP TABLE IF EXISTS metadata.fe;
+CREATE TABLE metadata.fe (
+  best_score float64
+  fi_out json
+  time_exec float64
+  df_shape varchar(10)
+);
+COMMENT ON TABLE metadata.fe IS 'Almacena metadata de los datos ingestados';
+
+
+/* ================================= metadata almacenamiento ================================= */
+
+DROP TABLE IF EXISTS metadata.almacenamiento;
+CREATE TABLE metadata.almacenamiento (
+  
+);
+COMMENT ON TABLE metadata.almacenamiento IS 'Almacena metadata de los datos ingestados';
+>>>>>>> cfe1888f5aa406b88e4b818dc4ae8c283d0f9d1c
