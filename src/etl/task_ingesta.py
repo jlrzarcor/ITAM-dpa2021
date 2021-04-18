b4b76fbe4098b4b0c13c19a30b5d839b1278ffda
Jose Luis R. Zarate <jlrzarate@hotmail.com>
2021-04-18 18:18:46 -0500

    adpatation for task metadata

--- src/etl/task_ingesta.py
+++ src/etl/task_ingesta.py
@@ -2,8 +2,10 @@
 
 # importing packages
 import luigi
+import pandas as pd
 import json
 from datetime import datetime
+import os
 
 # importing custom libraries
 import src.pipeline.ingesta_almacenamiento as ial
@@ -23,17 +25,26 @@ class TaskIngest(luigi.Task):
         
     def run(self):
     # Formatting date parameters into date-string
-        str_date = str(datetime.date(datetime(self.year, self.month, self.day))) + 'T00:00:00.000'
+        str_date = str(datetime.date(datetime(self.year, self.month, self.day)))
         
     # SODAPY. Connecting client to Socrata and Ingestion -> json.dumps
         client = ial.get_client()
 
         if self.flg_i0_c1 == 0:
-            data_i = ial.ingesta_inicial(client, delta_date = str_date , limit = 300000)
+            data_i = ial.ingesta_inicial(client, delta_date = str_date + 'T00:00:00.000', limit = 300000)
         else:
-            data_i = ial.ingesta_consecutiva(client, delta_date = str_date, limit = 1000)
+            data_i = ial.ingesta_consecutiva(client, delta_date = str_date + 'T00:00:00.000', limit = 1000)
+
+        json_data = json.dumps(data_i, indent=4)            
+            
+    # Lineage. Creating Metadata @ .csv file
+        str_file = str_date + ".csv"
+        output_path = "src/temp/metadata/ingestion/type={}/".format(self.flg_i0_c1)
+        os.makedirs(output_path, exist_ok = True)
         
-        json_data = json.dumps(data_i, indent=4)
+        dic_par = {'year':str(self.year),'month':str(self.month),'day':str(self.day),'flg_i0_c1':str(self.flg_i0_c1)}
+        df = pd.DataFrame({'fecha': [str_date], 'param_exec': [json.dumps(dic_par)],'usuario': ['luigi'], 'num_regs_ing': [len(data_i)]})
+        df.to_csv(output_path + str_file, index=False, header=False)
         
     # Call output
         with self.output().open('w') as output_file:
