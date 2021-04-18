03f7f169e37b4fe0ec74a0970f7bd7b95c017bee
Octavio <ofuentesitam@gmail.com>
2021-04-18 13:20:57 -0500

    Update create_metadata_tables.sql

--- sql/create_metadata_tables.sql
+++ sql/create_metadata_tables.sql
@@ -16,3 +16,38 @@ CREATE TABLE metadata.ingestion (
   num_regs_ing int
 );
 COMMENT ON TABLE metadata.ingestion IS 'Almacena metadata de los datos ingestados';
+
+
+/* ================================= metadata cleaning ================================= */
+
+DROP TABLE IF EXISTS metadata.cleaning;
+CREATE TABLE metadata.cleaning (
+  num_registros_antes_limpieza int
+  num_registros_despues_limpieza int
+  valores_nulos int
+  tamano_df_limpio int
+  fecha_ejecucion date
+  executer varchar(10)
+);
+COMMENT ON TABLE metadata.cleaning IS 'Almacena metadata de los datos ingestados';
+
+
+/* ================================= metadata feature engineering ================================= */
+
+DROP TABLE IF EXISTS metadata.fe;
+CREATE TABLE metadata.fe (
+  best_score float64
+  fi_out json
+  time_exec float64
+  df_shape varchar(10)
+);
+COMMENT ON TABLE metadata.fe IS 'Almacena metadata de los datos ingestados';
+
+
+/* ================================= metadata almacenamiento ================================= */
+
+DROP TABLE IF EXISTS metadata.almacenamiento;
+CREATE TABLE metadata.almacenamiento (
+  
+);
+COMMENT ON TABLE metadata.almacenamiento IS 'Almacena metadata de los datos ingestados';
