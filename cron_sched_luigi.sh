c2b36be95d104bf0866c38eb06f65604c2077ee5
Jose Luis R. Zarate <jlrzarate@hotmail.com>
2021-05-08 15:26:25 -0500

    new try with eval export PATH="/home/ubuntu/.pyenv/shims:${PATH}" export PYENV_SHELL=bash source '/home/ubuntu/.pyenv/libexec/../completions/pyenv.bash' command pyenv rehash 2>/dev/null pyenv() {   local command   command="${1:-}"   if [ "$#" -gt 0 ]; then     shift   fi
      case "$command" in
      activate|deactivate|rehash|shell)
        eval "$(pyenv "sh-$command" "$@")";;
      *)
        command pyenv "$command" "$@";;
      esac
    }, did not activate pyenv

--- cron_sched_luigi.sh
+++ cron_sched_luigi.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd ~/rep-proj-dpa
+eval "$(pyenv init -)"
+
 pyenv activate itam_dpa
+
+cd ~/dpa-proj
+export PGSERVICEFILE=${HOME}/.pg_service.conf
+export PGSERVICE=jl_cfi
 export PYTHONPATH=$PWD
-PYTHONPATH="." luigi --module 'src.etl.task_almacenamiento' TaskStore --local-scheduler --bucket 
-echo $PYTHONPATH > testcron_path.txt 
-pip freeze > testcron_pipfreeze.txt
+
+PYTHONPATH="." luigi --module 'src.etl.task_almacenamiento' TaskStore --local-scheduler
+
+echo $PYTHONPATH > time_test.txt 
+pip freeze > crontest_pipfreeze.txt
