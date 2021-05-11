39a1730f5039ab7ceac92a81d975b64b33c2b188
Jose Luis R. Zarate <jlrzarate@hotmail.com>
2021-05-10 23:38:09 -0500

    add RDS table cleaner

---
+++ sql/drop_tables.sql
@@ -0,0 +1,16 @@
+/*
+ * ITAM-Data - Product Architecture - Master Liliana Millán
+ * Project: Chicago Food Inspections - Team 05
+ * Script: Drop single tables...
+ */
+
+DROP TABLE IF EXISTS metadata.ingestion
+DROP TABLE IF EXISTS metadata.almacenamiento;
+DROP TABLE IF EXISTS metadata.cleaning;
+DROP TABLE IF EXISTS metadata.feat_eng;
+DROP TABLE IF EXISTS metadata.unittest;
+DROP TABLE IF EXISTS metadata.training;
+DROP TABLE IF EXISTS metadata.modelo;
+DROP TABLE IF EXISTS metadata.biasfair;
+DROP TABLE IF EXISTS procdata.limpieza;
+DROP TABLE IF EXISTS procdata.feat_eng;
