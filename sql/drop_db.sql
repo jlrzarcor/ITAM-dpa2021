77f576169eefd480ee9ad0b7d03c1d64acf8c601
Jose Luis R. Zarate <jlrzarate@hotmail.com>
2021-05-25 22:54:38 -0500

    sql script to drop db

---
+++ sql/drop_db.sql
@@ -0,0 +1,9 @@
+/*
+ * ITAM-Data - Product Architecture - Master Liliana Millán
+ * Project: Chicago Food Inspections - Team 05
+ * Script: Drop databases...
+ */
+
+
+DROP DATABASE IF EXISTS db_cfi CASCADE;
+DROP DATABASE IF EXISTS chicagofoodinsp CASCADE;
